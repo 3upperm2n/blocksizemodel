eee36043990bb8732c7e54f58f2bf1373890c110
Leiming Yu <leimingyu830@gmail.com>
2017-04-13 15:49:16 -0400

    rename

--- vector_add/gentrace.sh
+++ vector_add/gentrace.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-nvprof --print-gpu-trace --csv ./vectorAdd_bs32  2> bs32trace.csv
-nvprof --metrics all --csv ./vectorAdd_bs32  2> bs32.csv
+nvprof --print-gpu-trace --csv ./vectorAdd_bs32  2> trace32.csv
+nvprof --metrics all --csv ./vectorAdd_bs32  2> metrics32.csv
 
-nvprof --print-gpu-trace --csv ./vectorAdd_bs64  2> bs64trace.csv
-nvprof --metrics all --csv ./vectorAdd_bs64  2> bs64.csv
+nvprof --print-gpu-trace --csv ./vectorAdd_bs64  2> trace64.csv
+nvprof --metrics all --csv ./vectorAdd_bs64  2> metrics64.csv
 
-nvprof --print-gpu-trace --csv ./vectorAdd_bs128  2> bs128trace.csv
-nvprof --metrics all --csv ./vectorAdd_bs128  2> bs128.csv
+nvprof --print-gpu-trace --csv ./vectorAdd_bs128  2> trace128.csv
+nvprof --metrics all --csv ./vectorAdd_bs128  2> metrics128.csv
 
-nvprof --print-gpu-trace --csv ./vectorAdd_bs256  2> bs256trace.csv
+nvprof --print-gpu-trace --csv ./vectorAdd_bs256  2> trace256.csv
 nvprof --metrics all --csv ./vectorAdd_bs256  2> bs256.csv
 
-nvprof --print-gpu-trace --csv ./vectorAdd_bs512 2> bs512trace.csv
-nvprof --metrics all --csv ./vectorAdd_bs512 2> bs512.csv
+nvprof --print-gpu-trace --csv ./vectorAdd_bs512 2> trace512.csv
+nvprof --metrics all --csv ./vectorAdd_bs512 2> metrics512.csv
 
-nvprof --print-gpu-trace --csv ./vectorAdd_bs1024 2> bs1024trace.csv
-nvprof --metrics all --csv ./vectorAdd_bs1024 2> bs1024.csv
+nvprof --print-gpu-trace --csv ./vectorAdd_bs1024 2> trace1024.csv
+nvprof --metrics all --csv ./vectorAdd_bs1024 2> metrics1024.csv
