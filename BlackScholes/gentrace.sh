66a8f3781aef18def77bf3cae4d639ad26450c16
Leiming Yu <leimingyu830@gmail.com>
2017-04-14 19:14:16 -0400

    blackscholes update

--- BlackScholes/gentrace.sh
+++ BlackScholes/gentrace.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-cd bs128 && make && nvprof --metrics all --csv ./BlackScholes 2> bs128.csv && mv bs128.csv ../  && cd ../
-cd bs64 && make && nvprof --metrics all --csv ./BlackScholes 2> bs64.csv && mv bs64.csv ../  && cd ../
-cd bs32 && make && nvprof --metrics all --csv ./BlackScholes 2> bs32.csv && mv bs32.csv ../  && cd ../
-cd bs16 && make && nvprof --metrics all --csv ./BlackScholes 2> bs16.csv && mv bs16.csv ../  && cd ../
+cd bs128 && make clean && make
+nvprof --print-gpu-trace --csv  ./BlackScholes 2> trace128.csv
+nvprof --metrics all --csv 		./BlackScholes 2> metrics128.csv 
+mv *.csv ../  && cd ../
+
+cd bs64 && make clean && make
+nvprof --print-gpu-trace --csv  ./BlackScholes 2> trace64.csv
+nvprof --metrics all --csv 		./BlackScholes 2> metrics64.csv 
+mv *.csv ../  && cd ../
+
+cd bs32 && make clean && make
+nvprof --print-gpu-trace --csv  ./BlackScholes 2> trace32.csv
+nvprof --metrics all --csv 		./BlackScholes 2> metrics32.csv 
+mv *.csv ../  && cd ../
+
+cd bs16 && make clean && make
+nvprof --print-gpu-trace --csv  ./BlackScholes 2> trace16.csv
+nvprof --metrics all --csv 		./BlackScholes 2> metrics16.csv 
+mv *.csv ../  && cd ../
