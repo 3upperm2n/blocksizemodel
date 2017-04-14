1db4188ec43612fe975661755be553d11cd83d9e
Leiming Yu <leimingyu830@gmail.com>
2017-04-13 23:30:58 -0400

    trace

--- matMul/gentrace.sh
+++ matMul/gentrace.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-nvprof --metrics all --csv ./mm4     2> bs16.csv
-nvprof --metrics all --csv ./mm8     2> bs64.csv
-nvprof --metrics all --csv ./mm16    2> bs256.csv
-nvprof --metrics all --csv ./mm32    2> bs1024.csv
+nvprof --print-gpu-trace --csv ./mm4     2> trace16.csv
+nvprof --metrics all     --csv ./mm4     2> metrics16.csv
+
+nvprof --print-gpu-trace --csv ./mm8     2> trace64.csv
+nvprof --metrics all     --csv ./mm8     2> metrics64.csv
+
+nvprof --print-gpu-trace --csv ./mm16     2> trace256.csv
+nvprof --metrics all     --csv ./mm16     2> metrics256.csv
+
+nvprof --print-gpu-trace --csv ./mm32     2> trace1024.csv
+nvprof --metrics all     --csv ./mm32     2> metrics1024.csv
+
